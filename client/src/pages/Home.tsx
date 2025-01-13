00e0d95aff127d58cb15d2f8bc62521c46aefe44
DanSias <dan.sias@gmail.com>
2025-01-13 10:52:09 -0500

    Create Home.tsx
    Home page for app

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to My App</h1>
+      <p className="text-gray-700 mt-4">This is the home page of your app.</p>
+    </div>
+  );
+};
+
+export default Home;
